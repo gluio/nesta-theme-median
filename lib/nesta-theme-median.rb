5da6391f8d9372c8f04d86af2dd395c734c89dba
Glenn Gillen <me@glenngillen.com>
2015-05-04 15:36:47 +1000

    Override Nesta theme loading.

--- lib/nesta-theme-median.rb
+++ lib/nesta-theme-median.rb
@@ -5,4 +5,18 @@ module Nesta
     module Median
     end
   end
+
+  class Path
+    class << self
+      alias_method :pre_median_themes, :themes
+    end
+
+    def self.themes(*args)
+      if Nesta::Config.theme && args[0] == "median"
+        File.expand_path(File.join(*args[1..-1]), Nesta::App.root)
+      else
+        pre_median_themes(*args)
+      end
+    end
+  end
 end
