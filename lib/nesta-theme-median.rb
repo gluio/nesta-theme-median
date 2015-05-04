3f820be2bbc171f098fb3193122144a37e51528e
Glenn Gillen <me@glenngillen.com>
2015-05-04 17:02:21 +1000

    Encapsulate monkeypatch into it's own file.

--- lib/nesta-theme-median.rb
+++ lib/nesta-theme-median.rb
@@ -1,23 +1,9 @@
-require "nesta/path"
 require "nesta-theme-median/version"
+require "nesta-theme-median/path"
 
 module Nesta
   module Theme
     module Median
-    end
-  end
-
-  class Path
-    class << self
-      alias_method :pre_median_themes, :themes
-    end
-
-    def self.themes(*args)
-      if Nesta::Config.theme && args[0] == "median"
-        File.expand_path(File.join(*args[1..-1]), File.dirname(__FILE__) + "/..")
-      else
-        pre_median_themes(*args)
-      end
     end
   end
 end
---
+++ lib/nesta-theme-median/path.rb
@@ -0,0 +1,16 @@
+require "nesta/path"
+module Nesta
+  class Path
+    class << self
+      alias_method :pre_median_themes, :themes
+    end
+
+    def self.themes(*args)
+      if Nesta::Config.theme && args[0] == "median"
+        File.expand_path(File.join(*args[1..-1]), File.dirname(__FILE__) + "/..")
+      else
+        pre_median_themes(*args)
+      end
+    end
+  end
+end
