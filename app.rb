033b4fe01c0b35765979826809e22855484e47a3
Glenn Gillen <me@glenngillen.com>
2015-05-20 12:47:37 +1000

    Separate out markdown and render options for RedCarpet.

--- app.rb
+++ app.rb
@@ -1,17 +1,23 @@
 module Nesta
   class Page < FileModel
     def convert_to_html(format, scope, text)
+      render_options = {
+        prettify: true,
+        safe_links_only: true,
+        with_toc_data: true
+      }
       markdown_options = {
+        renderer: Redcarpet::Render::HTML.new(render_options),
         autolink: true,
         disable_indented_code_blocks: true,
         fenced_code_blocks: true,
+        footnotes: true,
         highlight: true,
         no_intra_emphasis: true,
         quote: true,
         strikethrough: true,
         superscript: true,
-        tables: true,
-        with_toc_data: true
+        tables: true
       }
       text = add_p_tags_to_haml(text) if @format == :haml
       template = Tilt[format].new(nil, 1, markdown_options) { text }
