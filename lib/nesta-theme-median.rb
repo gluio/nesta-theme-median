d3507dea6dfbb95f5c5757cea979b9bb10cb523c
Glenn Gillen <me@glenngillen.com>
2015-06-29 14:19:08 +1000

    Switch to new theme loading mechanism.

--- lib/nesta-theme-median.rb
+++ lib/nesta-theme-median.rb
@@ -1,6 +1,6 @@
 require 'nesta-contentfocus-extensions'
 require "nesta-theme-median/version"
-require "nesta-theme-median/page"
+require "nesta-theme-median/app"
 
 module Nesta
   module Theme
@@ -8,3 +8,12 @@ module Nesta
     end
   end
 end
+
+asset_path = File.expand_path("../assets", File.dirname(__FILE__))
+view_path = File.expand_path(asset_path + "/views")
+stylesheet_path = File.expand_path(asset_path + "/stylesheets")
+Nesta::ContentFocus::Paths.add_view_path(view_path)
+Nesta::ContentFocus::Paths.add_view_path(File.expand_path(view_path + "/median"))
+Nesta::ContentFocus::Paths.add_view_path(stylesheet_path)
+Nesta::ContentFocus::Paths.add_sass_path(stylesheet_path)
+
---
+++ lib/nesta-theme-median/app.rb
@@ -0,0 +1,7 @@
+require 'nesta-theme-median/helpers'
+require 'nesta-theme-median/page'
+module Nesta
+  class App
+    helpers Nesta::Theme::Median::Helpers
+  end
+end
